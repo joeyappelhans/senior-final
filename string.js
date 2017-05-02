26fa2657821b88d37b0cd522bb6f539959528bd1
joeyappelhans <9jappelhans@stpatrick.org>
2017-05-02 13:53:05 -0500

    worked more on functions

--- string.js
+++ string.js
@@ -39,27 +39,78 @@ exports.containsUpper =function(str){
         let hasUpper=false;
         try{
             for(let a=0; a<str.length; a++){
-                if(inRange(65-90)==true{
+                if(inRange(a,65,90)==true){
                     let hasUpper=true;
                     throw new PasswordMessage("has uppercase character");
                 }
                 else{
-                    throw new PasswordMessage("does not have uppercase letter")
+                    let hasUpper=false;
                 }
+            }
+            if(hasUpper==false){
+                throw new PasswordMessage("does not have uppercase letter");
+            }
+            catch(e){
+                console.log(e.message);
+                return hasUpper;
             }
         }
 }
 
 exports.containsLower =function(str){
-
+    let hasLower=false;
+    try{
+        for(let a=0; a<str.length; a++){
+            if(inRange(a,97,122)==true){
+                let hasLower=true;
+                throw new PasswordMessage("has lowercase character");
+            }
+            else{
+                let hasLower=false;
+            }
+        }
+        if(hasLower==false){
+            throw new PasswordMessage("does not have lowercase letter");
+        }
+        catch(e){
+            console.log(e.message);
+            return hasLower;
+        }
+    }
 }
-
 
 exports.containsNumerical =function(str){
-
+    let hasNumber=false;
+    try{
+        for(let a=0; a<str.length; a++){
+            if(inRange(a,48,57)==true){
+                let hasNumber=true;
+                throw new PasswordMessage("has number");
+            }
+            else{
+                let hasNumber=false;
+            }
+        }
+        if(hasNumber==false){
+            throw new PasswordMessage("does not have number");
+        }
+        catch(e){
+            console.log(e.message);
+            return hasNumber;
+        }
+    }
 }
 
-
 exports.containsSpecial =function(str){
-
+    let hasSpecial=false;
+    let special= [33,64,35,36,37,94,38,42];
+    try{
+        for(let d=0; d<special.length; d++){
+            for(let e=0; e<special.length; e++){
+                if(d.charCodeAt(0)==e;){
+                    
+                }
+            }
+        }
+    }
 }
